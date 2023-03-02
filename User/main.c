813ca5faae5ac55e4088619eb722feec449fe9e3
HZD <2684791179@qq.com>
2023-03-02 12:30:36 +0800

    数码管显示温度

--- User/main.c
+++ User/main.c
@@ -2,34 +2,25 @@
 #include "smg.h"
 #include "138.h"
 #include "delay.h"
+#include "DS18B20.h"
 
 #include "STC15F2K60S2.h"
 
 #include "stdio.h"
-#include "math.h"
-
-#define max(a,b)(a>b?a:b)
-#define min(a,b)(a<b?a:b)
 
 void main()
 {
-	/*
-		
-	*/
-	//char str[] = "-114514-\0 ";
-	//char str[] = "88888888";
-	
+	char str[10];
+
 	// Init
 	Cls_Periphreal();
 	SMG_Init();
 	
 	// Start
-	//SMG_ShowStr(0, str);
-	
-	
 	
 	while(1)
 	{
-		
+		sprintf(str,"     %04.2f",rd_temperature()/16.0);
+		SMG_ShowStr(0, str);
 	}
 }
