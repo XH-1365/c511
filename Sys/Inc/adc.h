26c45c1d838031d1762f5fd45df499ca35b225c1
HZD <2684791179@qq.com>
2023-03-06 19:04:56 +0800

    A/D转换

---
+++ Sys/Inc/adc.h
@@ -0,0 +1,5 @@
+#include "STC15F2K60S2.h"
+#include "intrins.h"
+
+unsigned char adc_pcf8591(void);
+void init_pcf8591(void);
--- User/main.c
+++ User/main.c
@@ -1,16 +1,24 @@
 #include "sys.h"
+#include "smg.h"
+#include "adc.h"
 
-#include "STC15F2K60S2.h"
+/*------------------------*/
+#include "stdio.h"
 
 void main()
 {
+	char str[10];
+	unsigned char Rb2;
 	// Init
 	Cls_Periphreal();
-	
+	init_pcf8591();
+	SMG_Init();
 	// Start
-	
 	while(1)
 	{
 		// Loop
+		Rb2 = adc_pcf8591();
+		sprintf(str," %3d",(unsigned int)Rb2);
+		SMG_ShowStr(0, str);
 	}
 }
