005b63a20c1e34c915eba17d6b580269cda2e775
hawav <hwj101@163.com>
2023-03-13 21:28:04 +0800

    数码管显示小数点示例

--- User/main.c
+++ User/main.c
@@ -1,4 +1,5 @@
 #include "sys.h"
+#include "smg.h"
 
 #include "STC15F2K60S2.h"
 
@@ -8,9 +9,11 @@ void main()
 	Cls_Periphreal();
 	
 	// Start
+	SMG_ShowStr(".1 2.3.4.5-");
 	
 	while(1)
 	{
 		// Loop
+		SMG_Flash();
 	}
 }
