3f1c7fac3875b60edcda207b5b62067e7029ddc7
hawav <hwj101@163.com>
2023-03-13 21:27:10 +0800

    BREAKING CHANGE(smg): 移除对数码管亮度的支持

--- Sys/Inc/smg.h
+++ Sys/Inc/smg.h
@@ -4,9 +4,8 @@
 void SMG_Init(void);
 void SMG_Deinit(void);
 
-void SMG_ShowPattern(unsigned char pos, unsigned char pattern, unsigned int brightness);
-unsigned char SMG_ShowChar(unsigned char pos, char ch, unsigned int brightness);
-void SMG_ShowStr(unsigned char pos, char * str);
-void SMG_SetBrightness(unsigned char pos, unsigned int brightness);
+void SMG_ShowChar(unsigned char pos, char ch, bit dot);
+void SMG_ShowStr(char * str);
+void SMG_Flash();
 
 #endif // __SMG_H
--- Sys/smg.c
+++ Sys/smg.c
@@ -4,65 +4,16 @@
 
 #include "STC15F2K60S2.h"
 
-typedef struct
-{
-	char pattern;
-	unsigned int brightness;
-} SMG_Unit;
+unsigned char patterns[8] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-SMG_Unit units[8] = {0};
+const unsigned char SMG_NUMBERS[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
-void _SMG_SetTimeout(unsigned int timeout)
-{
-	// timeout = 0x4000 - (timeout >> 2);
-	timeout = ~timeout;
-	TL0 = timeout & 0xff;		//设置定时初值
-	TH0 = ((timeout >> 8) & 0xff);		//设置定时初值
-}
-
-void Timer0Init(void)		//5微秒@11.0592MHz
-{
-	AUXR |= 0x80;		//定时器时钟1T模式
-	TMOD &= 0xF0;		//设置定时器模式
-	TMOD |= 0x01;		//设置定时器模式
-	TL0 = 0xF5;		//设置定时初值
-	TH0 = 0xFF;		//设置定时初值
-	TF0 = 0;		//清除TF0标志
-	TR0 = 1;		//定时器0开始计时
-}
-
-void SMG_Init(void)
-{
-	char i;
-	for(i = 0; i < 8; ++i)
-	{
-		units[i].pattern = 0xff;
-		units[i].brightness = 0;
-	}
-	
-	Timer0Init();
-	EA = ET0 = 1;
-}
-
-void SMG_Deinit(void)
-{
-	TR0 = 0;
-}
-
-void SMG_ShowPattern(unsigned char pos, unsigned char pattern, unsigned int brightness)
-{
-	units[pos].pattern = pattern;
-	units[pos].brightness = brightness;
-}
-
-const unsigned char CHAR_PATTERN[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
-
-unsigned char SMG_ShowChar(unsigned char pos, char ch, unsigned int brightness)
+void SMG_ShowChar(unsigned char pos, char ch, bit dot)
 {
 	char pattern = 0xff;
 	if (ch >= '0' && ch <= '9')
 	{
-		pattern = CHAR_PATTERN[ch - '0'];
+		pattern = SMG_NUMBERS[ch - '0'];
 	}
 	else
 	{
@@ -71,58 +22,37 @@ unsigned char SMG_ShowChar(unsigned char pos, char ch, unsigned int brightness)
 				pattern = 0xbf;
 				break;
 			case '.':
-				if (pos > 0)
-					--pos;
-				pattern = units[pos].pattern & ~0x80;
+				pattern = ~0x80;
 				break;
 		}
 	}
-	SMG_ShowPattern(pos, pattern, brightness);
-
-	return pos;
+	if (dot)
+	{
+		pattern &= ~0x80;
+	}
+	patterns[pos] = pattern;
 }
 
-void SMG_SetBrightness(unsigned char pos, unsigned int brightness)
-{
-	units[pos].brightness = brightness;
-}
-
-void SMG_ShowStr(unsigned char pos, char * str)
+void SMG_ShowStr(char * str)
 {
 	char i;
-	for(i = 0; pos < 8; ++i)
+	for(i = 0; i < 8; ++i)
 	{
-		if (!str[i])
-			return;
-		pos = SMG_ShowChar(pos, str[i], 60000) + 1;
+		bit nextIsDot = str[1] == '.';
+		SMG_ShowChar(i, *str, nextIsDot);
+		if (*str)
+		{
+			++str;
+			if (nextIsDot) ++str;
+		}
 	}
 }
 
-void SMG_TurnOff()
-{
-	SendTo(SMG_POS, 0);
-}
-
-void _SMG_Flash() interrupt 1
+void SMG_Flash()
 {
 	static char i = 0;
-	static bit on = 0;
-
-	if (on)
-	{
-		SendTo(SMG_CHAR, 0xff);
-		_SMG_SetTimeout(-units[i].brightness);
-		i = (i + 1) % 8;
-	}
-	else
-	{
-		if (units[i].brightness)
-		{
-			SendTo(SMG_POS, 1 << i);
-			SendTo(SMG_CHAR, units[i].pattern);
-		}
-		_SMG_SetTimeout(units[i].brightness);
-	}
-	
-	on = !on;
+	SendTo(SMG_CHAR, 0xff);
+	SendTo(SMG_POS, 1 << i);
+	SendTo(SMG_CHAR, patterns[i]);
+	if(++i >= 8) i = 0;
 }
